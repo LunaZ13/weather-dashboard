2e5265aca0079fa39179740ccf210a46ce4942ad
inmarluna <inmarluna@Inmars-MacBook-Pro.local>
2021-10-03 17:41:42 -0500

    weather api data response

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # weather-dashboard
+
+## Discription: 
+I was tasked with creating a timed quiz from scratch. The quiz should be multiple choice JavaScript fundamentals and should also store high score, so the user can compare to others.
+
+## Installation:
+Open in browser or click on the following link. https://lunaz13.github.io/coding-quiz/
+
+## Usage:
+See demo and instructions below.
+
+![Coding Quiz](https://user-images.githubusercontent.com/86627336/134993455-a92119de-ec28-4110-b993-56c10f08edc9.gif)
+
+
+## Instructions:
+1. Click the "Start Quiz" button
+    * The timer will start to run from 75 seconds.
+    * The user will be presented with a series of questions.
+
+2. Questions Section
+    * A series of multiple choice questions will appear.
+    * If the questions is answered incorrectly, 10 seconds will deducted from the timer.
+    * After each question, the user will be alerted if they are "correct" or "wrong".
+
+3. When all questions have been answered or the timer runs out, the game is over.
+
+4. When the game is over, the user's score will be displayed. Then they can input their initials and click "submit" to save their score.
+
+5. The user can then click on the "view high scores" button to view their score and others.
+
+6. The user can then click on the "clear high score" button to clear the saved scores, or click on the "play again" button to restart the quiz.
+
+## Built With:
+* HTML
+* CSS
+* JavaScript
+
+
+## Credits
+Created by Inmar Luna :grinning:
+
+## License 
+
+Copyright (c) [2021] [Inmar Luna]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- homepage.js
+++ homepage.js
@@ -1,21 +1,19 @@
 var APIKEY = "&appid=fd752dc15f7e56d500006bb657ee9154";
 
-var queryURL = "https:///api.openweathermap.org/data/2.5/weather?q=" + city + APIKEY;
+var queryURL = "https:///api.openweathermap.org/data/2.5/weather?q=" + "Saint Paul" + APIKEY;
 var queryURLFive = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + APIKEY;
 var searchedCities = [];
 
 var inputEl = document.getElementById("#city-input");
 var searchEl = document.getElementById("#search-button");
 var cityEl = document.getElementById("#city");
-var temperatureEl = document.getElementById("#temperature");
-var humidityEl = document.getElementById("#humidity");
-var windEl = document.getElementById("#wind");
-var uvIndexEl = document.getElementById("#uv-index");
-var weatherDiv = document.getElementById("#current-weather")
-var cityHistory = document.getElementById("#searchedCities")
+var weatherDiv = document.getElementById("#current-weather");
+var cityHistory = document.getElementById("#searchedCities");
+var weatherContainer = document.getElementById("#weather-conditions");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
+    console.log(event);
     var city = inputEl.value.trim();
     if (city) {
         getWeather(city);
@@ -25,21 +23,53 @@ var formSubmitHandler = function(event) {
         alert("Please enter a valid City.");
     }
     searchedCities();
-
-}
+};
 
 var getWeather = function(city) {
-    var queryURL;
-    var queryURLFive;
 
-    fetch(apiURL).then(function(response){
-        if (response.ok) {
-            response.json().then(function(data) {
-                weatherDiv(data, city);
-            });
-        };
-    });
+    fetch(queryURL)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+
+        var kelvinData = response.main.temp; 
+        console.log(kelvinData);
+
+        var pressure = response.main.pressure;
+        console.log(pressure);
+                    
+        });
+    };
     
+
+getWeather();
+
+var weatherDiv = function (weather, city) {
+    weatherContainer.textContent = "";
+    cityEl.textContent = city;
+
+    var weatherIcon = document.createElement("img")
+    weatherIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+    cityEl.appendChild(weatherIcon);
+
+    var temperatureEl = document.createElement("span");
+    temperatureEl.textContent = "Temperature: " + weather.main.temp + "°F";
+    temperatureEl.classList = "list-group-item"
+
+    weatherContainer.appendChild(temperatureEl);
+
+    var humidityEl = document.createElement("span");
+    humidityEl.textContent = "Humidity: " + weather.main.humidity + "%";
+    humidityEl.classList = "list-group-item"
+
+    weatherContainer.appendChild(humidityEl);
+
+    var windSpeedEl = document.createElement("span");
+    windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + "MPH";
+    windSpeedEl.classList = "list-group-item"
+
+    weatherContainer.appendChild(windSpeedEl);
+
 }
 
 
@@ -50,6 +80,4 @@ var getWeather = function(city) {
 
 
 
-
-
-searchEl.addEventListener("submit", formSubmitHandler)
+searchEl.addEventListener("click", formSubmitHandler);
